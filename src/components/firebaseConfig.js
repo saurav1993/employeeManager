242fc4ee078308af85931c3fef868975fa43519d
saurav ganguly <surgang81993@gmail.com>
2018-03-15 13:07:13 +0530

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default{
-    apiKey: "AIzaSyDvr12quAGeLZ1HTwbS_2urb46sLZC8YUw",
-    authDomain: "vuedemotraversy.firebaseapp.com",
-    databaseURL: "https://vuedemotraversy.firebaseio.com",
-    projectId: "vuedemotraversy",
-    storageBucket: "vuedemotraversy.appspot.com",
-    messagingSenderId: "182608463114"
-}
